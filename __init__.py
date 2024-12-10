e73dfa6b7f4167a5c360abc7d234fe34af29e71e
Yogurt <yogurt-7771@outlook.com>
2024-12-10 09:15:55 +0800

    fix: add logging when loading nodes
    Signed-off-by: Yogurt <yogurt-7771@outlook.com>

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+print("Loading Yogurt Nodes...")
+
 import inspect
 
 from .yogurt_nodes import *
@@ -10,3 +12,5 @@ for name, obj in list(globals().items()):
     if inspect.isclass(obj) and hasattr(obj, "_NODE_NAME"):
         NODE_CLASS_MAPPINGS[name] = obj
         NODE_DISPLAY_NAME_MAPPINGS[name] = f"{obj._NODE_NAME} (Yogurt Nodes)"
+
+print("Yogurt Nodes loaded!")
