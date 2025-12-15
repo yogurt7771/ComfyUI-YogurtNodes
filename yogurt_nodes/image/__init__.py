3007baf1a69b629497ca8b919cea8cf235713c5c
Yogurt Wang <yogurt-7771@outlook.com>
2025-12-15 14:41:10 +0800

    feat: add GetImageSize class for retrieving image size information
    Signed-off-by: Yogurt Wang <yogurt-7771@outlook.com>

--- yogurt_nodes/image/__init__.py
+++ yogurt_nodes/image/__init__.py
@@ -2,4 +2,5 @@ from .add_text_to_image import AddTextToImage  # noqa: F401
 from .batch_image import BatchImages  # noqa: F401
 from .crop_by_mask import ImageCropByMask  # noqa: F401
 from .poisson_blend import PoissonBlend  # noqa: F401
-from .scale_image import ImageScaleToTotalPixelsAdvanced
+from .scale_image import ImageScaleToTotalPixelsAdvanced  # noqa: F401
+from .get_image_size import GetImageSize  # noqa: F401
---
+++ yogurt_nodes/image/get_image_size.py
@@ -0,0 +1,35 @@
+
+class GetImageSize:
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE", "INT", "INT", "INT", "INT", "INT", "INT", "INT", "FLOAT", "FLOAT",)
+    RETURN_NAMES = ("images", "count", "width", "height", "channels", "longest_side", "shortest_side", "aspect_ratio", "megapixels",)
+    FUNCTION = "execute"
+
+    OUTPUT_NODE = False
+
+    _NODE_NAME = "Get Image Size"
+    CATEGORY = "YogurtNodes/Image"
+    DESCRIPTION = "Get image size information."
+
+    def execute(self, image) -> tuple[int, int]:
+        batch_size = image.shape[0] if image.dim() >= 1 else 0
+        height = image.shape[1] if image.dim() >= 2 else 0
+        width = image.shape[2] if image.dim() >= 3 else 0
+        channels = image.shape[3] if image.dim() >= 4 else 1
+
+        longest_side = max(width, height)
+        shortest_side = min(width, height)
+
+        aspect_ratio = width / height if height != 0 else 0
+        megapixels = (width * height) / 1_000_000
+
+        # Send progress text to display size on the node
+        return width, height, batch_size, channels, longest_side, shortest_side, aspect_ratio, megapixels
